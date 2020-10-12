4386a8cb4078696ef84de1f8deb0019ce10a731e
Thiago de Castro Savanachi <thiago12castro@gmail.com>
2020-10-11 21:17:03 -0300

    first commit

---
+++ VSCode/Index.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* TODO REVISAR CALCULO DE AMORTIZAÇÕA */
+/* FIXME CORRIGIR CALCULO DAS MÉDIAS DOS ALUNOS */ 
+
+/**@author Thiago <thiago12castro@gmail.com> */
+phpinfo();  
+
+/**
+ * Class constructor.
+ */
